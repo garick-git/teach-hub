10df99b218cfedd69d252784ce5f279400e0b7a5
garick-git <martin.mendez@erpchile.com>
2024-05-21 14:39:55 -0500

    added Clerk auth.

---
+++ app/(routes)/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Settings() {
+  return (
+    <main>
+      <div>
+        Settings
+      </div>
+    </main>
+  )
+}
---
+++ app/(routes)/student/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Student() {
+  return (
+    <main>
+      <div>
+        Student
+      </div>
+    </main>
+  )
+}
